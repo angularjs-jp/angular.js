f90e86e4003e983ac02cf719053027585e513d68
iseki <iseki.m.aa@gmail.com>
2013-02-15 14:40:14 +0900

    Revert "fix(angular.equals): relax the comparison for undefined properties"
    This reverts commit 3c2e1c5e4d12529b1d69a6173c38097527dccc4f.

--- test/AngularSpec.js
+++ test/AngularSpec.js
@@ -126,12 +126,12 @@ describe('angular', function() {
       expect(equals(['misko'], ['misko', 'adam'])).toEqual(false);
     });
 
-    it('should ignore undefined member variables during comparison', function() {
+    it('should ignore undefined member variables', function() {
       var obj1 = {name: 'misko'},
           obj2 = {name: 'misko', undefinedvar: undefined};
 
-      expect(equals(obj1, obj2)).toBe(true);
-      expect(equals(obj2, obj1)).toBe(true);
+      expect(equals(obj1, obj2)).toBe(false);
+      expect(equals(obj2, obj1)).toBe(false);
     });
 
     it('should ignore $ member variables', function() {
